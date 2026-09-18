8ec04ac87a0f8d8870c0dabee083661be1455321
sbooss <sbooss@users.noreply.github.com>
2026-09-17 22:28:02 -0300

    Remover sandbox - nenhum player funciona com ele

--- player.js
+++ player.js
@@ -156,7 +156,6 @@ class VideoPlayer {
             iframe.frameBorder = '0';
             iframe.allowFullscreen = true;
             iframe.allow = 'autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen';
-            iframe.sandbox = 'allow-scripts allow-same-origin allow-forms allow-presentation';
             iframe.className = 'video-iframe';
             iframe.loading = 'eager';
             iframe.style.opacity = '0';
