9cab484238350aa2912e919432b17261ab5fcc83
sbooss <sbooss@users.noreply.github.com>
2026-09-17 22:38:26 -0300

    Voltar Fembed primeiro com dublagem PT-BR, remover VidSrc

--- config.js
+++ config.js
@@ -2,19 +2,14 @@ const CONFIG = {
     EMBED: {
         PROVIDERS: [
             {
-                name: 'VidSrc',
-                movie: function(id) { return 'https://vidsrc.su/embed/movie/' + id; },
-                tv: function(id, s, e) { return 'https://vidsrc.su/embed/tv/' + id + '/' + s + '/' + e; }
+                name: 'Fembed',
+                movie: function(id) { return 'https://fembed.lol/filme/' + id; },
+                tv: function(id, s, e) { return 'https://fembed.lol/serie/' + id + '/' + s + '/' + e; }
             },
             {
                 name: 'CineSrc',
                 movie: function(id) { return 'https://cinesrc.st/embed/movie/' + id; },
                 tv: function(id, s, e) { return 'https://cinesrc.st/embed/tv/' + id + '?s=' + s + '&e=' + e; }
-            },
-            {
-                name: 'Fembed',
-                movie: function(id) { return 'https://fembed.lol/filme/' + id; },
-                tv: function(id, s, e) { return 'https://fembed.lol/serie/' + id + '/' + s + '/' + e; }
             }
         ]
     },
